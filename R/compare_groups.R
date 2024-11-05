c0f677820a1e04304fdb6474844d639137a9069b
Maryam Hasanzadehkiabi <maryam.hasanzadehkiabi@mail.utoronto.ca>
2024-11-05 18:38:40 -0500

    updated function and added more to vignette

--- R/compare_groups.R
+++ R/compare_groups.R
@@ -1,10 +1,16 @@
-#' Compare microbial communities between users and non-users
+#' Compare Oral microbial communities between substance users and non-users
 #' need to finish
-#' need to somehow demostrate and find data that belongs to substance users and non-users and differentiate them
+#' need to somehow demonstrate and find data that belongs to substance users and non-users and differentiate them
 #'
-#'
-#' @param data Microbiome data with user group info.
-#' @return Summary statistics of microbial differences.
+#' @param data Oral Microbiome data with a column for SubstanceUseType.
+#' @return A data frame summarizing species differences between groups.
 compare_groups <- function(data) {
-  # Code to compare microbial communities
+  # Example for sample data code: summarize species by SubstanceUseType
+  result <- data %>%
+    group_by(SubstanceUseType, Species) %>%
+    summarize(Count = sum(Count), .groups = "drop")
+
+  return(as.data.frame(result))
 }
+
+
