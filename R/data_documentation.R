ff3d6b8fe3f9d44275ead8833c1ecef20b8ddcd9
Maryam Hasanzadehkiabi <maryam.hasanzadehkiabi@mail.utoronto.ca>
2024-11-27 18:45:12 -0500

    added 2 sample data

---
+++ R/data_documentation.R
@@ -0,0 +1,24 @@
+#' Example Dataset 1: Microbial Taxonomic Data
+#'
+#' Simulated dataset of microbial taxa and their abundances in Control and Substance User groups.
+#'
+#' @format A data frame with 100 rows and 3 variables:
+#' \describe{
+#'   \item{Taxon}{Taxonomic group names (e.g., Taxon1, Taxon2, etc.).}
+#'   \item{Abundance}{Simulated microbial abundance values.}
+#'   \item{Group}{Group category: "Control" or "Substance Users".}
+#' }
+"sample_data"
+
+#' Example Dataset 2: Substance Use and Microbial Abundance
+#'
+#' Simulated dataset showing microbial abundance across different substance use types.
+#'
+#' @format A data frame with 100 rows and 3 variables:
+#' \describe{
+#'   \item{Substance}{Type of substance use (e.g., Alcohol, Cannabis, etc.).}
+#'   \item{Group}{Group category: "Control" or "Users".}
+#'   \item{Abundance}{Simulated microbial abundance values.}
+#' }
+"sample_data2"
+
